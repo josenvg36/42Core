655dcd18ba321ac61f569c84c1858036c234ae73
Jose Najul <jnajul@c1r7s1.42warsaw.pl>
2024-04-24 11:09:24 +0200

    minitalk

---
+++ 06minitalk/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnajul <jnajul@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 12:17:56 by jnajul            #+#    #+#             */
+/*   Updated: 2024/04/23 12:19:52 by jnajul           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+# include "libft.h"
+# include "ft_printf.h"
+
+#endif
